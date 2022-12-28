c291ea6c06f928d0c5b2f44787773f48f97fd5ff
Pablo Sastre <pablosastre@live.u-tad.com>
2022-12-28 18:43:07 +0100

    Modificacion 3

--- Git_Pablo_Sastre/src/paquete/PS.java
+++ Git_Pablo_Sastre/src/paquete/PS.java
@@ -4,7 +4,7 @@ public class PS {
 
 	public static void main(String[] args) {
 		System.out.println("Alumno:_Pablo_Sastre");
-		System.out.println("Primera modificacion desde Master");
+		System.out.println("Primera modificacion desde Master - Cambiado");
 	}
 
 }
