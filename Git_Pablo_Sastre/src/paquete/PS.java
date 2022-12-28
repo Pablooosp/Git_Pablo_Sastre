5d8971de5d8be7773688203e83251e4c58aa4055
Pablooosp <116873685+Pablooosp@users.noreply.github.com>
2022-12-28 19:07:53 +0100

    Update PS.java
    Modificacion 5

--- Git_Pablo_Sastre/src/paquete/PS.java
+++ Git_Pablo_Sastre/src/paquete/PS.java
@@ -4,12 +4,9 @@ public class PS {
 
 	public static void main(String[] args) {
 		System.out.println("Alumno:_Pablo_Sastre");
-<<<<<<< HEAD
 		System.out.println("Primera modificacion desde Master - Cambiado");
-=======
-		System.out.println("Primera modificacion desde Master");
 		System.out.println("Modificacion desde Dev");
->>>>>>> refs/heads/Dev
+		System.out.println("Modificacion desde GitHub");
 	}
 
 }
